8809873c26eed50b5854516c29eed98e3b721ded
Louis-gabriel Laplante <llaplant@c2r5p10.42quebec.com>
2022-07-08 10:57:24 -0400

    Wrote all of inception, time for testing and absorbing all of that information

---
+++ srcs/mariadb/script/initial_db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS wordpress;
+CREATE USER IF NOT EXISTS 'llaplant'@'%' IDENTIFIED BY 42;
+GRANT ALL PRIVILEGES ON wordpress.* TO 'llaplant'@'%';
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'root42';
---
+++ srcs/nginx/script/start.sh
@@ -0,0 +1,7 @@
+if [ ! -f /etc/ssl/certs/nginx.crt ]; then
+echo "nginx: ssl setting up";
+openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=CA/ST=Quebec/L=Quebec/O=wordpress/CN=llaplant.42.fr";
+echo "nginx: setup done";
+fi
+
+exec "$@"
