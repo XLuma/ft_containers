65977ad572d49af296cf8d3150bee1b0bc0e614a
Louis-gabriel Laplante <llaplant@c2r5p10.42quebec.com>
2022-07-22 09:09:56 -0400

    remove wp_siteurl

--- srcs/wordpress/script/wp-config.php
+++ srcs/wordpress/script/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'WP_SITEURL', 'llaplant.42.fr' );
+
 define( 'DB_NAME', 'wordpress' );
 
 /** Database username */
